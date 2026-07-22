5e9647d398aa2e7369ae52d5c7b788e205e69d99
김화균 <waffle@gimhwagyun-ui-MacBookAir.local>
2026-07-23 06:53:15 +0900

    fix: hide mobile stat units

--- src/components/molecules/StatCard/style.ts
+++ src/components/molecules/StatCard/style.ts
@@ -63,6 +63,12 @@ export const StatValue = styled.span`
   }
 `;
 
+export const Unit = styled.span`
+  ${media.mobile} {
+    display: none;
+  }
+`;
+
 export const StatLabel = styled.span`
   font-size: 16px;
   color: ${({ theme }) => theme.colors.gray600};
